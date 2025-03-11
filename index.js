37afd3275a57f5f178fa00b351fba15d3f757ce1
Neema <neema.edward@student.moringaschool.com>
2025-03-11 10:41:58 +0300

    adds aritmetic operations

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+console.log(multiply);
 
-// Write your code here
+const random = Math.floor(Math.random() * 10) + 1;
+console.log(random);
+
+const num3 = 16;
+const num4 = 6;
+const mod = num3 % num4;
+console.log(mod);
+
+const max = Math.max(1, 2, 3, 20);
+console.log(max);
+
